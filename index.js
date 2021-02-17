a605525c00c7311176249e1a55d36b58fb112fd6
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2021-02-17 15:46:43 -0700

    Finished 3rd task but just noticed that the 2nd is incorrect for some reason. Will go fix it now.

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ function is31Flavors(arr){
     }
  }
  
- console.log(is31Flavors(...originalFlavors));
+ console.log(is31Flavors(originalFlavors));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor. 
@@ -88,9 +88,13 @@ Use the addFlavor function below to do the following:
 */
 
 
-function addFlavor(/*your code here*/){
-   /*your code here*/
+function addFlavor(arr, str){
+    
+    arr.unshift(str);
+    return arr;
 }
+
+console.log(addFlavor(originalFlavors, 'Rainbow Sherbet'));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
